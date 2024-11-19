8de74059945ad5e48a7276c0ab4065195fc4e41f
James Parslow <james.parslow@sage.com>
2024-11-19 10:49:12 +0000

    chore: make warning message regarding installed carbon version clearer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import Box from "carbon-react/lib/components/box";
-import Button from "carbon-react/lib/components/button";
-import Typography from "carbon-react/lib/components/typography";
-import Textbox from "carbon-react/lib/components/textbox";
-import {
-  FlatTable,
-  FlatTableHead,
-  FlatTableBody,
-  FlatTableRow,
-  FlatTableHeader,
-  FlatTableCell,
-} from "carbon-react/lib/components/flat-table";
+import Box from "carbon-react/esm/components/box";
+import Typography from "carbon-react/esm/components/typography";
+import Message from "carbon-react/esm/components/message";
 
 const App = () => {
   return (
@@ -23,41 +14,12 @@ const App = () => {
       boxSizing="border-box"
     >
       <Typography variant="h1">Hello Carbon 👋</Typography>
-      <Typography variant="big">
-        Please remember to use the correct version of Carbon and
-        @sage/design-tokens as part of your demo. You can change this by
-        manually editing the <code>package.json</code>.
-      </Typography>
-      <Box display="flex" flexWrap="wrap" gap="var(--spacing200)">
-        <Button buttonType="primary">Primary</Button>
-        <Button buttonType="secondary">Secondary</Button>
-        <Button buttonType="tertiary">Tertiary</Button>
-        <Button destructive>Destructive</Button>
-      </Box>
-      <Textbox
-        label="Textbox"
-        inputHint="Hint text"
-        value="Jane Doe"
-        onChange={() => {}}
-      />
-      <FlatTable>
-        <FlatTableHead>
-          <FlatTableRow>
-            <FlatTableHeader>First Name</FlatTableHeader>
-            <FlatTableHeader>Surname</FlatTableHeader>
-          </FlatTableRow>
-        </FlatTableHead>
-        <FlatTableBody>
-          <FlatTableRow>
-            <FlatTableCell>John</FlatTableCell>
-            <FlatTableCell>Doe</FlatTableCell>
-          </FlatTableRow>
-          <FlatTableRow>
-            <FlatTableCell>Paul</FlatTableCell>
-            <FlatTableCell>Smith</FlatTableCell>
-          </FlatTableRow>
-        </FlatTableBody>
-      </FlatTable>
+      <Message variant="warning">
+        <Typography variant="b" m={0}>
+          Edit the <code>package.json</code> file manually to update the
+          versions of carbon-react and @sage/design-tokens as needed.
+        </Typography>
+      </Message>
     </Box>
   );
 };
