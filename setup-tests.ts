d84eb737fa09abc989749d0525678f8070bb5a5c
James Parslow <james.parslow@sage.com>
2025-02-24 09:44:04 +0000

    fix: update global mocks used in tests

--- setup-tests.ts
+++ setup-tests.ts
@@ -3,21 +3,19 @@ import { vi } from "vitest";
 
 // Place global setup code here, to be run before all tests...
 
-global.window.matchMedia =
-  global.window.matchMedia ||
-  ((query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: vi.fn(),
-    removeListener: vi.fn(),
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn(),
-  }));
+global.window.matchMedia = vi.fn().mockImplementation((query: string) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: vi.fn(),
+  removeListener: vi.fn(),
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn(),
+}));
 
-global.window.ResizeObserver = global.window.ResizeObserver || {
+global.window.ResizeObserver = vi.fn().mockResolvedValue({
   observe: vi.fn(),
   unobserve: vi.fn(),
   disconnect: vi.fn(),
-};
+});
