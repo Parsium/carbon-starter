26880fbd0648b84ecd6351fc1db42da1bca41b0f
James Parslow <james.parslow@sage.com>
2026-02-03 11:25:04 +0000

    refactor: use esm imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,20 @@
 import { createRoot } from "react-dom/client";
 
-import TokensWrapper from "carbon-react/lib/components/tokens-wrapper";
-import CarbonProvider from "carbon-react/lib/components/carbon-provider";
-import sageTheme from "carbon-react/lib/style/themes/sage";
-import GlobalStyle from "carbon-react/lib/style/global-style";
-import "carbon-react/lib/style/fonts.css";
+import TokensWrapper from "carbon-react/esm/components/tokens-wrapper";
+import CarbonProvider from "carbon-react/esm/components/carbon-provider";
+import sageTheme from "carbon-react/esm/style/themes/sage";
+import GlobalStyle from "carbon-react/esm/style/global-style";
+import "carbon-react/esm/style/fonts.css";
 
 import App from "./App";
 
 const root = createRoot(document.getElementById("app")!);
 
 root.render(
-  <>
-    <GlobalStyle />
-    <TokensWrapper>
-      <CarbonProvider validationRedesignOptIn theme={sageTheme}>
-        <App />
-      </CarbonProvider>
-    </TokensWrapper>
-  </>,
+  <TokensWrapper>
+    <CarbonProvider validationRedesignOptIn theme={sageTheme}>
+      <GlobalStyle />
+      <App />
+    </CarbonProvider>
+  </TokensWrapper>,
 );
